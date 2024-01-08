b875bbdf9773d71e1b828e2ee3cc32e665bb1d6a
Ogata-Kazuyoshi <kazuyoshi_ogata_ab@mail.toyota.co.jp>
2024-01-08 21:27:07 +0900

    本番環境URLも記載

--- app/src/api/axiosClient.ts
+++ app/src/api/axiosClient.ts
@@ -4,7 +4,7 @@ console.log('VITE_NODE_ENV : ', import.meta.env.VITE_NODE_ENV);
 const BaseURL =
   import.meta.env.VITE_NODE_ENV === 'production'
     ? // ? 'https://docker-knex-postgres.onrender.com/data'
-      'http://localhost:6200/api/v1/images/all'
+      'https://fullstack-docker-test.onrender.com/api/v1/images/all'
     : 'http://localhost:6200/api/v1/images/all';
 // const BaseURL = 'https://docker-knex-postgres.onrender.com/data';
 console.log('BaseURL : ', BaseURL);
